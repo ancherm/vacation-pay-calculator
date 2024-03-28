ec5a54f5e0c61e5682d1c69afd2f15ca29b456fb
Ancherm <andrewcherm2014@gmail.com>
2024-03-29 00:14:51 +0400

    add CalculatorController.java

---
+++ src/main/java/ru/chermashentsev/vacationpaycalculator/controllers/CalculatorController.java
@@ -0,0 +1,25 @@
+package ru.chermashentsev.vacationpaycalculator.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.chermashentsev.vacationpaycalculator.services.CalculatorService;
+
+import java.time.LocalDate;
+
+@RestController
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/calculate")
+    public double calculatePayVacation(@RequestParam double salaryForYear,
+                                       @RequestParam int countDays,
+                                       @RequestParam(required = false) String startVacationDay) {
+
+        return calculatorService.calculatePayVacation(salaryForYear, countDays, startVacationDay);
+    }
+}
